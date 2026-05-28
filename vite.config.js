53bf2d3f50c91fb16d743c2a1b29109ffd279086
danih.morais <daniel_assis_morais@hotmail.com>
2026-05-28 08:37:21 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,12 @@ export default defineConfig({
   plugins: [react()],
   clearScreen: false,
   server: {
+    host: "0.0.0.0",
     port: 1420,
     strictPort: true,
+    watch: {
+      ignored: ["**/src-tauri/**"]
+    }
   },
   envPrefix: ["VITE_", "TAURI_"],
   build: {
